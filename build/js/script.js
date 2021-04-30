8d637f8edfa97fa0784eedefcb2479ee9d5e981e
kelter-js <spawnkillerqw@gmail.com>
2021-04-30 21:50:01 +0400

    Добавляет шапку для десктопа, частичную реализацию шапки для таблет версии

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,16 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+const NAVIGATION_ELEMENT = document.querySelector('.main-nav');
+const TOGGLE_NAVIGATION_BUTTON = document.querySelector('.main-nav__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+NAVIGATION_ELEMENT.classList.remove('main-nav--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+const onClickToggleNavigation = () => {
+  if (NAVIGATION_ELEMENT.classList.contains('main-nav--closed')) {
+    NAVIGATION_ELEMENT.classList.remove('main-nav--closed');
+    NAVIGATION_ELEMENT.classList.add('main-nav--opened');
+    } else {
+      NAVIGATION_ELEMENT.classList.add('main-nav--closed');
+      NAVIGATION_ELEMENT.classList.remove('main-nav--opened');
   }
-});
+}
+
+TOGGLE_NAVIGATION_BUTTON.addEventListener('click', onClickToggleNavigation);
